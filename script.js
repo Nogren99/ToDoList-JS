d4b93f04fa560dc51bb3b0b5114e55ed517ff078
Nogren99 <nogren23@gmail.com>
2022-10-24 23:10:38 -0300

    Save without button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let items = []
 let addButton = document.getElementById("add-button")
 
 addButton.addEventListener("click",addToDoItem)
@@ -7,16 +8,33 @@ let inputBox = document.getElementById("todo-entry-box")
 function addToDoItem(){
 
     let itemText = inputBox.value
-    newToDoItem(itemText)
+    newToDoItem(itemText,false)
 
     inputBox.value="" //para formatear el cuadro
 }
 
 let toDoList = document.getElementById("todo-list")
 
-function newToDoItem(text){  //creamos elemento de la lista 
+function newToDoItem(text,completed){  //creamos elemento de la lista 
     let toDoItem = document.createElement("li")
     toDoItem.innerText = text
+
+    //carga storage
+    if(completed){
+        toDoItem.classList.add("completed")
+        
+    }
+    //guarda storage
+    let itemInfo = {
+        task: toDoItem.innerText,
+        completed: toDoItem.classList.contains("completed")
+    }
+    items.push(itemInfo)
+    localStorage.setItem("list",JSON.stringify(items))
+
+
+    
+
 
     toDoList.append(toDoItem)
     toDoItem.addEventListener("click",toggleItemState) //le agregamos para que se quede esperando un click que eecute toggle
@@ -30,6 +48,7 @@ function toggleItemState(){
     }else{
         this.classList.add("completed")  // el elemento de la li pasa a tener class="completed"
     }
+    saveList();
 }
 
 
@@ -45,24 +64,27 @@ function clearCompleted(){
     while(completedItems.length>0){
         completedItems[0].remove();
     }
+    saveList();
 }
 
 //Eliminar Lista
 
 let emptyButton = document.getElementById("empty-button")
-
 emptyButton.addEventListener("click",emptyList)
 
 function emptyList(){
     toDoList.innerHTML=""
+    saveList();
 }
 
 
 //Guardar lsita en storage
 
+/*
+<button id="save-button" class="pretty-button">Guardar</button>
 let saveButton = document.getElementById("save-button")
 saveButton.addEventListener("click",saveList)
-
+*/
 function saveList(){
     let items = []
 
@@ -71,7 +93,7 @@ function saveList(){
         
         let itemInfo = {
             task: item.innerText,
-            completed: item.classList.contains("compleeted")
+            completed: item.classList.contains("completed")
         }
 
         items.push(itemInfo)
@@ -87,9 +109,8 @@ function loadList(){
     let list = JSON.parse(localStorage.getItem("list")) // traigo un array de objetos
 
     for(let i=0 ;i<list.length;i++){
-        newToDoItem(list[i].task)
+        newToDoItem(list[i].task,list[i].completed)
     }
-
 
 }
 
